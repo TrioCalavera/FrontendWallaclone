86f228c81bd1a34256d31199d2a00d9fdce5dbc4
Basilio <basilio.uvinas@gmail.com>
2022-06-23 19:02:25 +0200

    Feat: Arreglar class -> ClassName del Main.js

--- frontendwallaclone/src/components/layout/Main.js
+++ frontendwallaclone/src/components/layout/Main.js
@@ -3,10 +3,10 @@ import product from "../../images/products-1.jpg";
 const Main = () => {
   return (
     <section className="popular-deals section bg-gray">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="section-title">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="section-title">
               <h2>Trending Adds</h2>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas,
@@ -15,60 +15,60 @@ const Main = () => {
             </div>
           </div>
         </div>
-        <div class="row">
+        <div className="row">
           {/* <!-- offer 01 --> */}
-          <div class="col-lg-12">
-            <div class="trending-ads-slide">
-              <div class="col-sm-12 col-lg-4">
+          <div className="col-lg-12">
+            <div className="trending-ads-slide">
+              <div className="col-sm-12 col-lg-4">
                 {/* <!-- product card --> */}
-                <div class="product-item bg-light">
-                  <div class="card">
-                    <div class="thumb-content">
-                      {/* <!-- <div class="price">$200</div> --> */}
+                <div className="product-item bg-light">
+                  <div className="card">
+                    <div className="thumb-content">
+                      {/* <!-- <div className="price">$200</div> --> */}
                       <a href="single.html">
                         <img
-                          class="card-img-top img-fluid"
+                          className="card-img-top img-fluid"
                           src={product}
-                          alt="Card image cap"
+                          alt="Card cap"
                         />
                       </a>
                     </div>
-                    <div class="card-body">
-                      <h4 class="card-title">
+                    <div className="card-body">
+                      <h4 className="card-title">
                         <a href="single.html">11inch Macbook Air</a>
                       </h4>
-                      <ul class="list-inline product-meta">
-                        <li class="list-inline-item">
+                      <ul className="list-inline product-meta">
+                        <li className="list-inline-item">
                           <a href="single.html">
-                            <i class="fa fa-folder-open-o"></i>Electronics
+                            <i className="fa fa-folder-open-o"></i>Electronics
                           </a>
                         </li>
-                        <li class="list-inline-item">
+                        <li className="list-inline-item">
                           <a href="#">
-                            <i class="fa fa-calendar"></i>26th December
+                            <i className="fa fa-calendar"></i>26th December
                           </a>
                         </li>
                       </ul>
-                      <p class="card-text">
+                      <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipisicing
                         elit. Explicabo, aliquam!
                       </p>
-                      <div class="product-ratings">
-                        <ul class="list-inline">
-                          <li class="list-inline-item selected">
-                            <i class="fa fa-star"></i>
+                      <div className="product-ratings">
+                        <ul className="list-inline">
+                          <li className="list-inline-item selected">
+                            <i className="fa fa-star"></i>
                           </li>
-                          <li class="list-inline-item selected">
-                            <i class="fa fa-star"></i>
+                          <li className="list-inline-item selected">
+                            <i className="fa fa-star"></i>
                           </li>
-                          <li class="list-inline-item selected">
-                            <i class="fa fa-star"></i>
+                          <li className="list-inline-item selected">
+                            <i className="fa fa-star"></i>
                           </li>
-                          <li class="list-inline-item selected">
-                            <i class="fa fa-star"></i>
+                          <li className="list-inline-item selected">
+                            <i className="fa fa-star"></i>
                           </li>
-                          <li class="list-inline-item">
-                            <i class="fa fa-star"></i>
+                          <li className="list-inline-item">
+                            <i className="fa fa-star"></i>
                           </li>
                         </ul>
                       </div>

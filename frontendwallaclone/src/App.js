b4d142c9aa603af155c574f9d4c3af90cf4b7327
Basilio <basilio.uvinas@gmail.com>
2022-06-23 19:35:58 +0200

    Feat: Crear componente LoginPage

--- frontendwallaclone/src/App.js
+++ frontendwallaclone/src/App.js
@@ -4,11 +4,14 @@ import Main from "./components/layout/Main";
 import Navbar from "./components/layout/Navbar";
 import Subfooter from "./components/layout/Subfooter";
 
+import LoginPage from "./components/login/LoginPage";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeaderPage />
+      <LoginPage />
       <Main />
       <FooterPage />
       <Subfooter />

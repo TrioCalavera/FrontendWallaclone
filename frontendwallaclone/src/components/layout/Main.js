312494ed22558873c858c73b0fe831cf83b4ee71
Basilio <basilio.uvinas@gmail.com>
2022-06-23 19:38:33 +0200

    Feat: Crear Componente AdCard, y maquetación de la misma

--- frontendwallaclone/src/components/layout/Main.js
+++ frontendwallaclone/src/components/layout/Main.js
@@ -1,4 +1,4 @@
-import product from "../../images/products-1.jpg";
+import AdCard from "../ads/AdCard";
 
 const Main = () => {
   return (
@@ -15,69 +15,9 @@ const Main = () => {
             </div>
           </div>
         </div>
-        <div className="row">
-          {/* <!-- offer 01 --> */}
-          <div className="col-lg-12">
-            <div className="trending-ads-slide">
-              <div className="col-sm-12 col-lg-4">
-                {/* <!-- product card --> */}
-                <div className="product-item bg-light">
-                  <div className="card">
-                    <div className="thumb-content">
-                      {/* <!-- <div className="price">$200</div> --> */}
-                      <a href="single.html">
-                        <img
-                          className="card-img-top img-fluid"
-                          src={product}
-                          alt="Card cap"
-                        />
-                      </a>
-                    </div>
-                    <div className="card-body">
-                      <h4 className="card-title">
-                        <a href="single.html">11inch Macbook Air</a>
-                      </h4>
-                      <ul className="list-inline product-meta">
-                        <li className="list-inline-item">
-                          <a href="single.html">
-                            <i className="fa fa-folder-open-o"></i>Electronics
-                          </a>
-                        </li>
-                        <li className="list-inline-item">
-                          <a href="#">
-                            <i className="fa fa-calendar"></i>26th December
-                          </a>
-                        </li>
-                      </ul>
-                      <p className="card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Explicabo, aliquam!
-                      </p>
-                      <div className="product-ratings">
-                        <ul className="list-inline">
-                          <li className="list-inline-item selected">
-                            <i className="fa fa-star"></i>
-                          </li>
-                          <li className="list-inline-item selected">
-                            <i className="fa fa-star"></i>
-                          </li>
-                          <li className="list-inline-item selected">
-                            <i className="fa fa-star"></i>
-                          </li>
-                          <li className="list-inline-item selected">
-                            <i className="fa fa-star"></i>
-                          </li>
-                          <li className="list-inline-item">
-                            <i className="fa fa-star"></i>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        {/* Productos */}
+        <div className="trending-ads-slide row">
+          <AdCard />
         </div>
       </div>
     </section>

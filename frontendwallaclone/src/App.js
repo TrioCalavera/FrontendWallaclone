b081c740fbaf780f5e16a9d5c8ba4c552eb7a2ad
Basilio <basilio.uvinas@gmail.com>
2022-06-23 19:37:32 +0200

    Feat: Crear componente RegisterPage

--- frontendwallaclone/src/App.js
+++ frontendwallaclone/src/App.js
@@ -4,11 +4,14 @@ import Main from "./components/layout/Main";
 import Navbar from "./components/layout/Navbar";
 import Subfooter from "./components/layout/Subfooter";
 
+import RegisterPage from "./components/register/RegisterPage";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeaderPage />
+      <RegisterPage />
       <Main />
       <FooterPage />
       <Subfooter />

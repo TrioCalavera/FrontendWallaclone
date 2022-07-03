446387684f6f7c4cf7a3b0e9d899fa49cbaba740
Basilio <basilio.uvinas@gmail.com>
2022-07-03 13:59:20 +0200

    Feat: Crear rutas generales para Login, register, anuncios, detalle de anuncio, 404

--- frontendwallaclone/src/App.js
+++ frontendwallaclone/src/App.js
@@ -1,11 +1,30 @@
-import Layout from "./components/layout/Layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+// import Layout from "./components/layout/Layout";
 import ScrollTop from "./components/elements/scrollTo/ScrollTop";
+import LoginPage from "./components/login/LoginPage";
+import RegisterPage from "./components/register/RegisterPage";
+import AdsContent from "./components/ads/AdsContent";
+import AdDetails from "./components/ads/AdDetails";
+import NotFound from "./components/layout/NotFound";
 
 function App() {
   return (
     <div className="App">
       <ScrollTop />
-      <Layout />
+      <Routes>
+        {/* Rutas generales */}
+        <Route path="/" element={<AdsContent />} />
+        <Route path="/adverts" element={<AdsContent />} />
+        <Route path="/adverts/:adsId" element={<AdDetails />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Rutas error */}
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+      {/* <Layout /> */}
     </div>
   );
 }
--- frontendwallaclone/src/index.js
+++ frontendwallaclone/src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "../src/css/bootstrap.min.css";
 import "../src/css/style.css";
 import "../src/fonts/font-awesome/css/font-awesome.min.css";
-// import "../src/images/cta-background.jpg";
-// import "../src/images/subscription-back.jpg";
-// import "../src/images/hero.jpg";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );

077b383c02c38ab76fd5c537bdf9914b8766ab00
Basilio <basilio.uvinas@gmail.com>
2022-07-17 11:46:55 +0200

    Feat: Ajustar select para devolver datos y que se pueda traducir

--- frontendwallaclone/src/components/elements/selectTags/SelectTags.js
+++ frontendwallaclone/src/components/elements/selectTags/SelectTags.js
@@ -1,13 +1,20 @@
 import Select from "react-select";
 import { useState, useEffect } from "react";
 import { getTags } from "../../../api/service";
+import { useTranslation } from "react-i18next";
 
 const SelectTags = () => {
+  const { t } = useTranslation();
   const [tagsAd, setTagsAd] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState();
 
-  function capitalizeFirstLetter(string) {
+  const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
+
+  const handleSelect = (data) => {
+    setSelectedOptions(data);
+  };
 
   useEffect(() => {
     const execute = async () => {
@@ -22,7 +29,16 @@ const SelectTags = () => {
     execute();
   }, []);
 
-  return <Select isMulti options={tagsAd} />;
+  return (
+    <Select
+      placeholder={t("newadvert.select_category")}
+      value={selectedOptions}
+      onChange={handleSelect}
+      isSearchable={true}
+      isMulti
+      options={tagsAd}
+    />
+  );
 };
 
 export default SelectTags;

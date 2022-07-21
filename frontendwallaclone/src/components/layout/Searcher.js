95c9f244202b56b1ea7678bd8033dab9830dd7a0
Basilio <basilio.uvinas@gmail.com>
2022-07-21 22:37:02 +0200

    Feat: Petición con Tags

--- frontendwallaclone/src/components/layout/Searcher.js
+++ frontendwallaclone/src/components/layout/Searcher.js
@@ -1,57 +1,81 @@
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
-import { getCustomAds } from "../../api/service";
+import { getCustomAds, getTags } from "../../api/service";
+import Select from "react-select";
+import tools from "../../utils/tools";
 
 const Searcher = () => {
   const { t } = useTranslation();
-  // const [customAds, setCustomAds] = useState([]);
+
+  //State petición Tags
+  const [tagsAd, setTagsAd] = useState([]);
+
+  const [tags, setTags] = useState([]);
+
+  //State nutrir select tags
+  const [selectedOptions, setSelectedOptions] = useState();
+
   const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const data = "?name=wii";
-  // useEffect(() => {
-  //   const execute = async () => {
-  //     // setIsLoading(true);
-  //     // const customAds = await getCustomAds(data);
-  //     // setCustomAds(customAds.result);
-  //     // console.log("customAds", customAds);
-  //     // setIsLoading(false);
-  //   };
-  //   execute();
-  //   return () => {};
-  // }, []);
+  // const [price, setPrice] = useState("");
+
+  //Manejar nutrir datos al select, los transforma y se los manda a los tags
+  const handleSelect = (data) => {
+    setSelectedOptions(data);
+    const tagsValue = data.map((tag) => tag.value);
+    setTags(tagsValue);
+  };
+
+  //Petición de tags y completa la información necesaria para tener Obj para el select
+  useEffect(() => {
+    const execute = async () => {
+      let tagsAd = await getTags();
+      let tagsNameLabel = tagsAd.map((tag) => ({
+        value: tag.name,
+        label: tools.capitalizeFirstLetter(tag.name),
+      }));
+      tagsAd = tagsNameLabel;
+      setTagsAd(tagsAd);
+    };
+    execute();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
-    formData.append("name", name);
-    formData.append("price", price);
+
+    //Validaciones Searcher
+    name && formData.append("name", name);
+    tags.length && formData.append("tags", tags);
     console.log(Object.fromEntries(formData));
 
     let query = new URLSearchParams(formData);
     console.log(decodeURIComponent(query.toString()));
 
     try {
-      // resetError();
-      // setIsLoading(true);
       const customAds = await getCustomAds(
         decodeURIComponent(query.toString())
       );
-      // setIsLoading(false);
     } catch (error) {
-      // setError(error);
-      // setIsLoading(false);
       console.log(error);
     }
+  };
+
+  const customStyles = {
+    option: () => ({
+      borderBottom: "5px dotted pink",
+      color: "blue",
+      padding: 20,
+    }),
   };
 
   return (
     <div className="advance-search">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-lg-12 col-md-12 align-content-center">
+          <div className="col-lg-12 col-md-12 ">
             <form onSubmit={handleSubmit}>
-              <div className="form-row">
-                <div className="form-group col-md-4">
+              <div className="form-col">
+                <div className="form-group col-md-12">
                   <input
                     type="text"
                     className="form-control my-2 my-lg-1"
@@ -61,23 +85,19 @@ const Searcher = () => {
                     placeholder={t("searcher.looking_for")}
                   />
                 </div>
-                <div className="form-group col-md-4">
-                  <input
-                    type="text"
-                    className="form-control my-2 my-lg-1"
-                    name="price"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                    placeholder="precios"
+                <div className="form-group col-md-12">
+                  <Select
+                    placeholder={t("newadvert.select_category")}
+                    value={selectedOptions}
+                    onChange={handleSelect}
+                    isSearchable={true}
+                    isMulti
+                    options={tagsAd}
+                    name={tags}
+                    style={customStyles}
+                    className="react-select__control react-select__value-container react-select__value-container"
+                    classNamePrefix="react-select"
                   />
-                </div>
-                <div className="form-group col-md-3">
-                  <select className="w-100 form-control mt-lg-1 mt-md-2">
-                    <option>Category</option>
-                    <option value="1">Top rated</option>
-                    <option value="2">Lowest Price</option>
-                    <option value="4">Highest Price</option>
-                  </select>
                 </div>
                 <div className="form-group col-md-2 align-self-center">
                   <button type="submit" className="btn btn-primary">
